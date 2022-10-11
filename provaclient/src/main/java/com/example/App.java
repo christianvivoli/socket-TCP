1b56d66ef53cbdce39b818f97e569f2dfef2f376
Christian Vivoli <christianvivoli04@gmail.com>
2022-10-11 17:31:34 +0200

    primo commit

--- provaclient/src/main/java/com/example/App.java
+++ provaclient/src/main/java/com/example/App.java
@@ -10,7 +10,8 @@ public class App
     {
         Socket s = new Socket("localhost", 3000);
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-
+        
+//!prima richiesta di messaggio
         BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Inserisci il messaggio:");
         String stringaUtente = tastiera.readLine();
@@ -18,11 +19,31 @@ public class App
         pr.println(stringaUtente);
         pr.flush();
 
+//!messaggio riguardate l altezza
+        BufferedReader tastiera2 = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Inserisci il altezza:");
+        String stringaUtente2 = tastiera2.readLine();
+        
+        pr.println(stringaUtente2);
+        pr.flush();
+
+//!messaggio riguardante il peso
+        BufferedReader tastiera3 = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Inserisci il peso:");
+        String stringaUtente3 = tastiera3.readLine();
+        
+        pr.println(stringaUtente3);
+        pr.flush();
+
+//!risposta server
+
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader br = new BufferedReader(in);
-
         String str = br.readLine();
         System.out.println("Server: " + str);
+        
+
+
         s.close();
     }
 }
--- provaserver/src/main/java/com/example/App.java
+++ provaserver/src/main/java/com/example/App.java
@@ -14,14 +14,34 @@ public class App
         Socket s = ss.accept();
         System.out.println("Client connesso");
 
+//?primo messaggio ricevuto
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader br = new BufferedReader(in);
         String str = br.readLine();
         System.out.println("Client: " + str);
 
+//?messagio riguardante l altezza
+
+        InputStreamReader in2 = new InputStreamReader(s.getInputStream());
+        BufferedReader br2 = new BufferedReader(in2);
+        String strgAltezza = br2.readLine();
+        float altezza= Float.valueOf(strgAltezza);
+
+//?messaggio riguardante il peso 
+
+        InputStreamReader in3 = new InputStreamReader(s.getInputStream());
+        BufferedReader br3 = new BufferedReader(in3);
+        String strgPeso = br3.readLine();
+        float peso= Float.valueOf(strgPeso);
+
+       //*prova funziona  System.out.println(peso/(altezza*altezza));
+
+//? mettere il risultato bmi
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("Ben arrivato");
+        pr.println("il bmi è: "+ peso/(altezza*altezza));
         pr.flush();
+
+
         s.close();
         ss.close();
 
